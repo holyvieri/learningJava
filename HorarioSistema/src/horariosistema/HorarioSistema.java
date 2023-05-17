4e12951bc35ebf1ed4039da68de56f7ace54467c
Ana Oliveira <holyvieri@icloud.com>
2023-05-17 19:33:37 -0300

    relógio de um sistema
    fase básica

---
+++ HorarioSistema/src/horariosistema/HorarioSistema.java
@@ -0,0 +1,12 @@
+package horariosistema;
+
+import java.util.Date;
+
+public class HorarioSistema {
+    public static void main(String[] args) {
+       Date relogio = new Date();
+        System.out.println("------ the current system time ------");
+        System.out.println(relogio.toString());
+    }
+    
+}
